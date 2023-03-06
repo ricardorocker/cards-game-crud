037e5deca89142a9f1bf360549da0521d83b1b4b
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-03-06 15:44:34 -0300

    Estrutura de projeto, localStorage service, home route
    •	Components
    o	Default layoult
    •	Services
    o	Local Storage
    •	Views
    o	Home
    o	Register Cards
    •	App-routing
    o	Path : ‘’ -> Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultLayoutComponent } from './components/default-layout/default-layout.component';
+import { HomeComponent } from './views/home/home.component';
+import { RegisterCardComponent } from './views/register-card/register-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DefaultLayoutComponent,
+    RegisterCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/register-card/register-card.component.ts
@@ -0,0 +1,12 @@
+import { LocalStorageService } from './../../services/local-storage.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-card',
+  templateUrl: './register-card.component.html',
+  styleUrls: ['./register-card.component.css']
+})
+export class RegisterCardComponent {
+
+  constructor() {}
+}
